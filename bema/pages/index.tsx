a0507d3d0e4a5af6e6ea9b6cb9c8157d6f6048af
arthurkyambadde <arthurkyambadde9@gmail.com>
2023-02-10 21:46:20 +0300

    fix: bema-ug name

--- bema/pages/index.tsx
+++ bema/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>BEMA</h1>
+        <h1>BEMA-UG</h1>
       </main>
     </>
   );
