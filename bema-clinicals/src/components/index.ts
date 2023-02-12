ea7feadb5025a94d6454360808c5872fd6aa414d
DESKTOP-G60BJ3E\Ssonko Jimmy <ssembatyadennis25@gmail.com>
2023-02-12 12:01:46 +0300

    style(Logo): make the Logo component responsive

--- bema-clinicals/src/components/index.ts
+++ bema-clinicals/src/components/index.ts
@@ -3,3 +3,16 @@ export { default as Navigation } from "./nav/Navigation";
 export { default as Banner } from "./banner/Banner";
 export { default as Hero } from "./hero/Hero";
 export { default as Button } from "./buttons/Button";
+
+// @if $breakpoint == phone {
+//     @media (max-width: 37.5em) { @content };    //600px
+// }
+// @if $breakpoint == tab-port {
+//     @media (max-width: 56.25em) { @content }; //900px
+// }
+// @if $breakpoint == tab-land {
+//     @media (max-width: 75em) { @content };  //1200px
+// }
+// @if $breakpoint == big-desktop {
+//     @media (min-width: 112.5em) { @content };   //1800
+// }
