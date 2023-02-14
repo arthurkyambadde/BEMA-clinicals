475709bdc42f61f52f90a0c39e36a879e59bb878
DESKTOP-G60BJ3E\Ssonko Jimmy <ssembatyadennis25@gmail.com>
2023-02-14 22:39:10 +0300

    style(Contact): display the contact cards in a row

--- bema-clinicals/src/containers/contact/Contact.tsx
+++ bema-clinicals/src/containers/contact/Contact.tsx
@@ -14,6 +14,18 @@ export default function Contact(): JSX.Element {
           card_title="Latest findings from a PREVAIL partnership contribute to the prevention of Ebola virus disease"
           card_text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere iure quam deserunt?Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere iure quam deserunt?"
         />
+
+        <ContactCard
+          card_image="assets/images/contact_2.png"
+          card_title="Covid-19 Clinical trial"
+          card_text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere iure quam deserunt?Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere iure quam deserunt?"
+        />
+
+        <ContactCard
+          card_image="assets/images/contact_3.png"
+          card_title="Meet Sandra NAluyima, Clinical Affairs Manager, Based in Masindi"
+          card_text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere iure quam deserunt?Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere iure quam deserunt?"
+        />
       </div>
     </section>
   );
