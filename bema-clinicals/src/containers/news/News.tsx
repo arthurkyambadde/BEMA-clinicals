b0c350117a14c177ce32e0a5a910723d74f2af0d
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-02-18 02:00:15 +0300

    Refactor news section styles

--- bema-clinicals/src/containers/news/News.tsx
+++ bema-clinicals/src/containers/news/News.tsx
@@ -9,11 +9,11 @@ export default function News(): JSX.Element {
         <h2 className="heading-secondary">
           Stay two steps ahead with BEMA-UG clinical TRIALS
         </h2>
-        <p className="header-text">
+        {/* <p className="header-text">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ullam
           quaerat, fugiat in ratione deleniti! Tempora doloremque quisquam eos
           ex, nesciunt temporibus{" "}
-        </p>
+        </p> */}
       </div>
       <div className={classes["news__image"]}>
         <Card
